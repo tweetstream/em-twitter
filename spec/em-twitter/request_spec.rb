3a46143a93ca90d732fb975aab2488989292775c
Steve Agalloco <steve.agalloco@gmail.com>
2011-12-20 13:46:45 -0500

    request specs

--- spec/em-twitter/request_spec.rb
+++ spec/em-twitter/request_spec.rb
@@ -1,4 +1,42 @@
 require 'spec_helper'
 
 describe EM::Twitter::Request do
+
+  describe '.new' do
+  end
+
+  describe '#http_request' do
+  end
+
+  describe '#to_s' do
+  end
+
+  describe '#proxy?' do
+  end
+
+  describe '#full_uri' do
+    context 'when using a proxy' do
+    end
+  end
+
+  describe '#proxy_uri' do
+  end
+
+  describe '#request_uri' do
+  end
+
+  describe '#uri_base' do
+  end
+
+  describe '#proxy_header' do
+  end
+
+  describe '#params' do
+  end
+
+  describe '#query' do
+  end
+
+  describe '#oauth_header' do
+  end
 end
