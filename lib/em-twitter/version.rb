54d878e072e3c861123ce02d40e3cdcebdab682e
Steve Agalloco <steve.agalloco@gmail.com>
2012-05-12 00:56:48 -0400

    make 0.1.0 the initial release version

--- lib/em-twitter/version.rb
+++ lib/em-twitter/version.rb
@@ -1,5 +1,5 @@
 module EventMachine
   module Twitter
-    VERSION = "0.0.1" unless defined?(EventMachine::Twitter::VERSION)
+    VERSION = "0.1.0" unless defined?(EventMachine::Twitter::VERSION)
   end
 end
