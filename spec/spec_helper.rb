3f55a568d1567cefbb3f0dde7d77acc5f1460f98
Steve Agalloco <steve.agalloco@gmail.com>
2012-05-12 23:46:49 -0400

    use the proper label for simplecov [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 unless ENV['CI']
   require 'simplecov'
   SimpleCov.start do
-    add_group 'Tweetstream', 'lib/em-twitter'
+    add_group 'EM-Twitter', 'lib/em-twitter'
     add_group 'Specs', 'spec'
   end
 end
