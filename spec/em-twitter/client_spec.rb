11160db8f43acadeb4428d8e4b7fb9448fde86ba
Steve Agalloco <steve.agalloco@gmail.com>
2012-09-25 12:41:41 -0400

    re-arrange Client tests

--- spec/em-twitter/client_spec.rb
+++ spec/em-twitter/client_spec.rb
@@ -23,17 +23,15 @@ describe EM::Twitter::Client do
       EM.stub(:connect).and_return(conn)
     end
 
-    context 'without a proxy' do
-      it 'connects to the configured host/port' do
-        EventMachine.should_receive(:connect).with(
-          test_options[:host],
-          test_options[:port],
-          EventMachine::Twitter::Connection,
-          kind_of(EM::Twitter::Client),
-          test_options[:host],
-          test_options[:port])
-        EM::Twitter::Client.connect(default_options)
-      end
+    it 'connects to the configured host/port' do
+      EventMachine.should_receive(:connect).with(
+        test_options[:host],
+        test_options[:port],
+        EventMachine::Twitter::Connection,
+        kind_of(EM::Twitter::Client),
+        test_options[:host],
+        test_options[:port])
+      EM::Twitter::Client.connect(default_options)
     end
 
     context 'when using a proxy' do
@@ -49,7 +47,7 @@ describe EM::Twitter::Client do
       end
     end
 
-    it "should not trigger SSL until connection is established" do
+    it "doesn't trigger SSL until connection is established" do
       connection = stub('EventMachine::Connection')
       EM.should_receive(:connect).and_return(connection)
       EM.should_not_receive(:start_tls)
