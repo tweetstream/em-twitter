0961b542d4a740dbbdae9e6286f5a0f7d242071d
Steve Agalloco <steve.agalloco@gmail.com>
2013-09-23 00:14:22 -0400

    bump version to 0.3.0

--- lib/em-twitter/version.rb
+++ lib/em-twitter/version.rb
@@ -1,5 +1,5 @@
 module EventMachine
   module Twitter
-    VERSION = "0.2.2" unless defined?(EventMachine::Twitter::VERSION)
+    VERSION = "0.3.0" unless defined?(EventMachine::Twitter::VERSION)
   end
 end
