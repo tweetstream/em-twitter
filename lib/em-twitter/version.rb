6155310211a040cd3cb66b7ebbde7402a021776f
Steve Agalloco <steve.agalloco@gmail.com>
2014-10-31 11:10:51 -0400

    version 0.3.5

--- lib/em-twitter/version.rb
+++ lib/em-twitter/version.rb
@@ -1,5 +1,5 @@
 module EventMachine
   module Twitter
-    VERSION = "0.3.4" unless defined?(EventMachine::Twitter::VERSION)
+    VERSION = "0.3.5" unless defined?(EventMachine::Twitter::VERSION)
   end
 end
